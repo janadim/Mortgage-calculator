e7cad4920c2bdef923ad26a6c88ded4e6cb7a7e5
janadim <jana.d.titarchuk@gmail.com>
2021-09-07 17:42:36 +0200

    include starter kit in project, adjust scss

---
+++ public/assets/js/main.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const costHouse = document.querySelector(".js-value");
+const savings = document.querySelector(".js-savings");
+const salary = document.querySelector(".js-salary");
+const radioInputs = document.querySelector(".js-10-years");
+const submitButton = document.querySelector(".js-submitButton");
+let negativeMessage = document.querySelector(".js-message");
+const fixedMonthCost = document.querySelector(".js-fixedMonthCost");
+const monthlyInterest = document.querySelector(".js-monthInterest");
+const totalCost = document.querySelector(".js-totalMonthCost");
+
+function correctDataInputs() {
+  console.log(costHouse.value);
+  if (costHouse.value === "" || salary.value === "") {
+    negativeMessage.innerHTML = `You have to fill in all the fields`;
+  } else {
+    console.log("everything is filled in");
+  }
+}
+
+function maxMortgage() {
+  const askedMortgage = costHouse.value - savings.value;
+  const allowedAmount = salary.value * 4;
+  const messageCalc = allowedAmount + savings.value;
+  if (askedMortgage <= allowedAmount) {
+    console.log(`you'll get ${messageCalc}`);
+  } else {
+    negativeMessage.innerHTML = `Sorry, but the maximal ammout in your case is ${messageCalc}`;
+    console.log(`Sorry, but the maximal ammout in your case is ${messageCalc}`);
+  }
+}
+
+// function fixedMonthCost() {
+//   maxMortgage();
+// }
+
+function handleClickSubmitButton() {
+  correctDataInputs();
+  maxMortgage();
+}
+
+submitButton.addEventListener("click", handleClickSubmitButton);
+
+//# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ const savings = document.querySelector(".js-savings");
 const salary = document.querySelector(".js-salary");
 const radioInputs = document.querySelector(".js-10-years");
 const submitButton = document.querySelector(".js-submitButton");
-const negativeMessage = document.querySelector(".js-message");
+let negativeMessage = document.querySelector(".js-message");
 const fixedMonthCost = document.querySelector(".js-fixedMonthCost");
 const monthlyInterest = document.querySelector(".js-monthInterest");
 const totalCost = document.querySelector(".js-totalMonthCost");
@@ -23,7 +23,6 @@ function maxMortgage() {
   const askedMortgage = costHouse.value - savings.value;
   const allowedAmount = salary.value * 4;
   const messageCalc = allowedAmount + savings.value;
-  negativeMessage = "";
   if (askedMortgage <= allowedAmount) {
     console.log(`you'll get ${messageCalc}`);
   } else {
