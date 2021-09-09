0e9c56b3a72909a741a99e8d7ee0ad7fad0660e1
janadim <jana.d.titarchuk@gmail.com>
2021-09-09 13:00:50 +0200

    run docs

--- src/js/main.js
+++ src/js/main.js
@@ -22,8 +22,12 @@ function correctDataInputs() {
 function maxMortgage() {
   const askedMortgage = costHouse.value - savings.value;
   const allowedAmount = salary.value * 4;
-  const messageCalc = allowedAmount + savings.value;
+  const messageCalc = parseInt(allowedAmount) + parseInt(savings.value);
+
+  console.log(messageCalc);
   if (askedMortgage <= allowedAmount) {
+    negativeMessage.innerHTML = `You can get: ${messageCalc}`;
+    // fixedMonthCost.innerHTML = monthCost;
     console.log(`you'll get ${messageCalc}`);
   } else {
     negativeMessage.innerHTML = `Sorry, but the maximal ammout in your case is ${messageCalc}`;
@@ -31,9 +35,16 @@ function maxMortgage() {
   }
 }
 
-// function fixedMonthCost() {
-//   maxMortgage();
+// const years = document.querySelector(".js-radioInterests");
+
+// function handleClickRadio(ev) {
+//   const radioTarget = ev.target;
+//   const monthCost = messageCalc / (radioTarget * 12);
+
+//   fixedMonthCost.innerHTML = monthCost;
 // }
+
+// years.addEventListener("click", handleClickRadio);
 
 function handleClickSubmitButton() {
   correctDataInputs();
@@ -42,5 +53,4 @@ function handleClickSubmitButton() {
 
 submitButton.addEventListener("click", handleClickSubmitButton);
 
-
-Aparece una casilla con el coste fijo mensual que sera igual a: (La cantidad pedida/[numero de anos de plazo*12meses])
+// Aparece una casilla con el coste fijo mensual que sera igual a: (La cantidad pedida/[numero de anos de plazo*12meses])
